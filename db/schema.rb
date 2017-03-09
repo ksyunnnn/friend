3447748ed21b123348686669a1fe4eb0b5b12eaf
ksyunnnn <l.o.s19kk@gmail.com>
2017-03-09 16:39:08 +0000

    add some columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303075427) do
+ActiveRecord::Schema.define(version: 20170309163827) do
 
   create_table "friends", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 20170303075427) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "facebook"
+    t.string   "twitter"
+    t.string   "photo"
   end
 
 end
