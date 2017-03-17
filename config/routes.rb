8e41e56fc3a972d0f186120acb266e1be635821b
ksyunnnn <l.o.s19kk@gmail.com>
2017-03-17 12:46:57 +0000

    finish creating session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'friends/index'
 
   get 'signup' => 'users#new'
+
+  # session
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :users
   root  'friends#index'
 end
